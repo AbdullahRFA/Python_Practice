91181172825aed2fddf4fca2fde3c4f00922a9fe
AbdullahRFA <shakibrybmn@gmail.com>
2024-10-31 10:22:15 +0600

    modified

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print(9+1)
+a=20
+b=34
+print(a+b)
+c=100
+
+
+def sum ( a,b ):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a+b
+
+sun()
