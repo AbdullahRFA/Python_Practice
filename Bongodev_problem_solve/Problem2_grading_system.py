2ed36758cdc9de58e56e8619f434442c6d53ce42
AbdullahRFA <shakibrybmn@gmail.com>
2024-10-31 20:24:46 +0600

    Add bongodev problem 2

---
+++ Bongodev_problem_solve/Problem2_grading_system.py
@@ -0,0 +1,10 @@
+number=int(input("Enter the number: "))
+
+if number>=90:
+    print("The grade is A+")
+elif number>=70:
+    print("The grade is A-")
+elif number>=50:
+    print("The grade is B")
+else:
+    print("Fail")
