b2d1a2ad2abacb47839b15a697f11da96bd052b5
AbdullahRFA <shakibrybmn@gmail.com>
2024-10-31 20:11:39 +0600

    bongodev prob1 is added

---
+++ Bongodev_problem_solve/problem1.py
@@ -0,0 +1,10 @@
+# Write a Python program that takes a string input from the user and checks if Enjoy available in
+# the input string:
+# "Hello, world! Hello everyone. Welcome to the world of Python. Enjoy coding in Python."
+
+problem1=input("Input a srtings: ")
+
+if "Enjoy" in problem1:
+    print("Enjoy is available in given string")
+else:
+    print("Enjoy is not availabe in given string")
