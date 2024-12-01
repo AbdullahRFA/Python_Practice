20bd8251d970153bc13fbe1d71c2c0600b507d6f
AbdullahRFA <shakibrybmn@gmail.com>
2024-12-02 00:21:10 +0600

    added python file that describe class and instance attributes

---
+++ attributes/class_and_instance_attributes.py
@@ -0,0 +1,22 @@
+class person:
+    counter=0 #Class variable/attributes
+    def __init__(self,name,dept):
+        self.name=name #instance attributes
+        self.dept = dept #instance attributes
+        person.counter+=1 #class attributes increase by 1
+        self.count = person.counter #instance attributes
+    def display(self):
+        print(f"Name : {self.name}")
+        print(f"Department : {self.dept}")
+        print(f"Employee NO : {self.count}")
+        
+        
+p1=person('Abdullah','CSE')
+p2=person('Nazmus','IIT')
+p3=person('Sakib','Robotics')
+
+p1.display()
+print()
+p2.display()
+print()
+p3.display()
